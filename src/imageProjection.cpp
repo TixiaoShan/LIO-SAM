46ffec1f5d7298af752fce5f2dddb65eb503943c
TixiaoShan <tixiao.shan@gmail.com>
2020-07-15 10:02:52 -0400

    range calculation bug fix, thanks to @minwo0jung

--- src/imageProjection.cpp
+++ src/imageProjection.cpp
@@ -534,10 +534,10 @@ public:
             // if (thisPoint.z < -2.0)
             //     continue;
 
-            rangeMat.at<float>(rowIdn, columnIdn) = range;
-
             thisPoint = deskewPoint(&thisPoint, laserCloudIn->points[i].time); // Velodyne
             // thisPoint = deskewPoint(&thisPoint, (float)laserCloudIn->points[i].t / 1000000000.0); // Ouster
+
+            rangeMat.at<float>(rowIdn, columnIdn) = pointDistance(thisPoint);
 
             int index = columnIdn  + rowIdn * Horizon_SCAN;
             fullCloud->points[index] = thisPoint;
