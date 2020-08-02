7c9a94c66fa0dc3f4ef9955dee8cda0374d04373
TixiaoShan <tixiao.shan@gmail.com>
2020-08-01 22:15:17 -0400

    clean loop closure code

--- include/utility.h
+++ include/utility.h
@@ -5,6 +5,7 @@
 #include <ros/ros.h>
 
 #include <std_msgs/Header.h>
+#include <std_msgs/Float64MultiArray.h>
 #include <sensor_msgs/Imu.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/NavSatFix.h>
@@ -132,7 +133,7 @@ public:
     
     // Loop closure
     bool  loopClosureEnableFlag;
-    float loopClosureInterval;
+    float loopClosureFrequency;
     int   surroundingKeyframeSize;
     float historyKeyframeSearchRadius;
     float historyKeyframeSearchTimeDiff;
@@ -205,7 +206,7 @@ public:
         nh.param<float>("lio_sam/surroundingKeyframeSearchRadius", surroundingKeyframeSearchRadius, 50.0);
 
         nh.param<bool>("lio_sam/loopClosureEnableFlag", loopClosureEnableFlag, false);
-        nh.param<float>("lio_sam/loopClosureInterval", loopClosureInterval, 5.0);
+        nh.param<float>("lio_sam/loopClosureFrequency", loopClosureFrequency, 1.0);
         nh.param<int>("lio_sam/surroundingKeyframeSize", surroundingKeyframeSize, 50);
         nh.param<float>("lio_sam/historyKeyframeSearchRadius", historyKeyframeSearchRadius, 10.0);
         nh.param<float>("lio_sam/historyKeyframeSearchTimeDiff", historyKeyframeSearchTimeDiff, 30.0);
