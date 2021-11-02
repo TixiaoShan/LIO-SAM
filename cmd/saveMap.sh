a64b05e1216500d2356d0838c555112719743fce
Oscar <oscar.johansson@astazero.com>
2021-11-02 14:15:58 +0100

    fixed faulty projection

--- cmd/saveMap.sh
+++ cmd/saveMap.sh
@@ -13,7 +13,11 @@ destination: '/LIO-SAM_Maps/$folderName/'"
 
 python FillJson.py $HOME/"LIO-SAM_Maps/$folderName/GlobalMap.pcd"
 
+echo "Converting to Las file.."
+
 pdal pipeline $HOME/"LIO-SAM_Maps/$folderName/ConvertLasToPcdWithGeoRef.json"
+
+echo "Done."
 
 echo "Las info: "
 
