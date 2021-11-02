76e072424db003e1beba52c1d58ad8ef39e44414
Oscar <oscar.johansson@astazero.com>
2021-11-02 14:51:07 +0100

    Removed failty pitch value in static transform between frames odom and map

--- src/imuPreintegration.cpp
+++ src/imuPreintegration.cpp
@@ -88,7 +88,7 @@ public:
     {
         // static tf
         static tf::TransformBroadcaster tfMap2Odom;
-        static tf::Transform map_to_odom = tf::Transform(tf::createQuaternionFromRPY(0, 0.26, 0), tf::Vector3(0, 0, 0));
+        static tf::Transform map_to_odom = tf::Transform(tf::createQuaternionFromRPY(0, 0, 0), tf::Vector3(0, 0, 0));
         tfMap2Odom.sendTransform(tf::StampedTransform(map_to_odom, odomMsg->header.stamp, mapFrame, odometryFrame));
 
         std::lock_guard<std::mutex> lock(mtx);
